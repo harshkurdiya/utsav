67735e01fe0b282052add077d8d8cc1b0ac35405
harshkurdiya <157039232+harshkurdiya@users.noreply.github.com>
2024-01-19 15:19:54 +0530

    event

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll to section when a navigation link is clicked
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
